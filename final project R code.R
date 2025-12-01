3e9bce525df46d4c62f3c5bc694325fcd84ffc3c
Bis-the-nub <jui20010815@gmail.com>
2025-12-01 14:49:10 -0500

    cleaning data

--- final project R code.R
+++ final project R code.R
@@ -2,12 +2,82 @@
 library(tidyverse)
 library(ggplot2)
 
-## Loading and cleaning data
-bike_2019_jan = read_csv("2019/JC-201901-citibike-tripdata.csv") |> 
-  janitor::clean_names() |> 
-  select(
-    -start_station_id, -end_station_id, -bikeid, -starttime, -stoptime
-  ) |> 
+## Loading and cleaning data for 2019 ones
+bike_files_info = tibble(
+  month_code = sprintf("%02d", 1:12),
+  month_name = c(
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  )) |> 
   mutate(
-    gender = as.factor(gender)
+    file_path = paste0("2019/JC-2019", month_code, "-citibike-tripdata.csv")
   )
+
+process_bike_data =
+  function(file_path, month_name) {
+  if (!file.exists(file_path)) {
+    warning(paste("file does not exist, skipped：", file_path))
+    return(NULL)
+  }
+  
+  data =
+    read_csv(file_path) |>
+    janitor::clean_names() |>
+    select(
+      -start_station_id, -end_station_id, -bikeid, -starttime, -stoptime
+    ) |>
+    mutate(
+      gender = as.factor(gender),
+      month = month_name
+    )
+  
+  return(data)
+}
+
+list_of_bike_data =
+  bike_files_info |>
+  select(file_path, month_name) |>
+  purrr::pmap(.f = process_bike_data)
+
+all_bike_data_2019 =
+  bind_rows(list_of_bike_data)
+
+## Loading and cleaning data for 2020 ones
+bike_files_info = tibble(
+  month_code = sprintf("%02d", 1:12),
+  month_name = c(
+    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+  )) |> 
+  mutate(
+    file_path = paste0("2020/JC-2020", month_code, "-citibike-tripdata.csv")
+  )
+
+process_bike_data =
+  function(file_path, month_name) {
+    if (!file.exists(file_path)) {
+      warning(paste("file does not exist, skipped：", file_path))
+      return(NULL)
+    }
+    
+    data =
+      read_csv(file_path) |>
+      janitor::clean_names() |>
+      select(
+        -start_station_id, -end_station_id, -bikeid, -starttime, -stoptime
+      ) |>
+      mutate(
+        gender = as.factor(gender),
+        month = month_name
+      )
+    
+    return(data)
+  }
+
+list_of_bike_data =
+  bike_files_info |>
+  select(file_path, month_name) |>
+  purrr::pmap(.f = process_bike_data)
+
+all_bike_data_2020 =
+  bind_rows(list_of_bike_data)
